fd4defa113697716a0079c9dae9726b3d6bf76e9
Sajjad Roohandeh <Roohandehsredi6@gmail.com>
2026-02-22 23:17:05 +0330

    Added calculator btns UI

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,10 +1,29 @@
-import './calculator.css'
+import "./calculator.css";
 
 function Calculator() {
   return (
     <div className="calculator">
       <div className="display">2+2=4</div>
-      <div className="btns"></div>
+      <div className="btns">
+        <div className="btn">7</div>
+        <div className="btn">8</div>
+        <div className="btn">9</div>
+        <div className="btn multiplication">×</div>
+        <div className="btn">4</div>
+        <div className="btn">5</div>
+        <div className="btn">6</div>
+        <div className="btn addition">+</div>
+        <div className="btn">1</div>
+        <div className="btn">2</div>
+        <div className="btn">3</div>
+        <div className="btn division">÷</div>
+        <div className="btn zero">0</div>
+        <div className="btn">.</div>
+        <div className="btn substraction">-</div>
+        <div className="btn clear">AC</div>
+        <div className="btn delete">D</div>
+        <div className="btn equal">=</div>
+      </div>
     </div>
   );
 }
